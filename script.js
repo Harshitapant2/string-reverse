7887f2ee8231d975d28039929b641c484fefff26
Harshitapant2 <pantharshita2006@gmail.com>
2024-11-10 16:21:27 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let input=document.getElementyById('cityname');
+let searchbtn=document.getElementById('seaechbtn');
+let cityNameElement=document.getElementById('city');
+let temp=document.getElementById('temp');
+let description=document.getElementById('description');
+let humidity=document.getElementById('humidity');
+let wind=document.getElementById('wind');
+let weatherIcon=document.getElementById('weather-icon');
